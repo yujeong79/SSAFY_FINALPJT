8d6116d59c12b429e3826df795fe0c7a0262ce54
김유정 <aabbc0908>
2024-11-21 09:08:16 +0900

    commit

--- a11_springboot_kimyujeong_kimhyeonrae/a11_Springboot_KimYuJeong_KimHyeonRae/src/main/resources/sql/MainServiceSchema.sql
+++ a11_springboot_kimyujeong_kimhyeonrae/a11_Springboot_KimYuJeong_KimHyeonRae/src/main/resources/sql/MainServiceSchema.sql
@@ -7,9 +7,14 @@ DROP TABLE IF EXISTS `review`;
 CREATE TABLE `review` (
     `review_id` INT PRIMARY KEY AUTO_INCREMENT,
     `email` VARCHAR(50) NOT NULL, -- 카카오 API에 맞게 수정
+    `nickname` VARCHAR(50) NOT NULL,
     `video_id` VARCHAR(50) NOT NULL,        
-    `content` TEXT NOT NULL
+    `content` TEXT NOT NULL,
+    `regist_day` DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO `review` (`email`, `nickname`, `video_id`, `content`)
+VALUES ('aabbc0908@naver.com', '김유정', 'AVCDES', '너무 힘들다 진자');
 
 commit;
 
